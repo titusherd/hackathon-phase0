6920e6bcb7dbc6766f273c5342d8796d78ed6a19
Frisy Novel <98008631+freezyyyyyy@users.noreply.github.com>
2023-01-12 18:40:07 +0700

    pembagian function

--- beken.js
+++ beken.js
@@ -1,13 +1,13 @@
 /**
- * 
+ * Code untuk bagian back end
  */
 
 let loginDb = [
-    { 
-        email: 'ini@monkeychimp.co.id', 
+    {
+        email: 'ini@monkeychimp.co.id',
         password: 'password',
         userId: 'eh812932'
-    }, 
+    },
     {
         email: 'magus@typemoon.co.id',
         password: 'saskeh',
@@ -42,17 +42,19 @@ let userDb = [
         userId: 'magusPinterz',
         username: 'Rin Tosahka',
         email: 'magus@typemoon.co.id',
-        age: 26,
+        age: 16,
         status: [
             {
+                statsId: '001',
                 timestamp: '04/12/2021',
-                message: 'Hari ini gw ketemu mantan dan gw denger dia baru putus',
+                message: 'haloo',
                 mood: 'happy',
                 quotes: 'Jangan Bahagia dulu'
             },
             {
+                statsId: '002',
                 timestamp: '14/01/2022',
-                message: 'Hari ini gw ketemu mantan dan gw denger dia baru putus',
+                message: 'gagal ujan',
                 mood: 'happy',
                 quotes: 'Jangan Bahagia dulu'
             }
@@ -60,12 +62,63 @@ let userDb = [
     },
 ];
 
-let mail = loginDb[0].email;
-let pass = loginDb[0].password;
-
-for (let i = 0; i < userDb.length; i++) {
-    const perUser = userDb[i];
-    let perNode = userDb[i].status;
-    console.log(perUser.username, perUser.age);
-    console.log(perNode);
+let moodDb = {
+    happy: [],
+    sad: [],
+    plain: []
 }
+
+
+/**
+ * 
+ * Proses backend mencangkup :   mainFunction -> submitSign -> login -> getDashboard -> showDashboard  
+ *                              -> selectMood -> tulisKeluhan
+ */
+
+// main function
+function main() {
+
+}
+
+
+// bagian gateway keeping
+function submitSign(email, password) {
+
+}
+
+function login(email, password) {
+
+}
+
+
+// bagian dalam sesudah log in
+
+function getDashboard(id) {
+    let dashboard = {};
+
+    for (let i = 0; i < userDb.length; i++) {
+        const perUser = userDb[i];
+
+        if (perUser.userId === id) {
+            dashboard = perUser;
+        }
+    }
+    return dashboard;
+}
+
+function showDashboard(dashboard) {
+    let nama = dashboard.username;
+    let umur = dashboard.age;
+
+    return [nama, umur];
+}
+
+function tulisSuasana(mood){
+    // penentuan warna pada DOM
+}
+
+let mood = moodDb['plain'];
+let id = loginDb[0].userId;
+let dashboard = getDashboard(id);
+
+console.log(showDashboard(dashboard));
